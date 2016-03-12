c3f18528a1dc61d9bbc0d570aace80d9a7f26503
Danny Lagrouw <danny.lagrouw@trivento.nl>
2016-03-12 17:26:00 +0100

    Add extra typings.

---
+++ typings/normalizr.d.ts
@@ -0,0 +1,12 @@
+declare module Normalizr {
+  export class Schema {
+    constructor(name: String)
+  }
+  export function arrayOf(obj: any);
+  export function normalize(obj: Object, schema: any);
+}
+
+declare module 'normalizr' {
+  export = Normalizr
+}
+
